ef2df4a6fc3fe27a28f7b45dd079e761994c3187
ile <ile@Ileana>
2017-07-20 16:09:48 +0300

    modify syso

--- GitTest/src/foo/StudentConsumer.java
+++ GitTest/src/foo/StudentConsumer.java
@@ -8,7 +8,7 @@ public class StudentConsumer {
 		s1.setName("Ion");
 		s1.setAge(24);
 		
-		System.out.println("Hello "+s1.getName());
+		System.out.println("Hello student "+s1.getName());
 	}
 	
 }
